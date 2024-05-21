78aaa4267a95e8015a24a0eee1e893dd8c8cc083
dev1d123 <dhuamanio@unsa.edu.pe>
2024-05-20 23:23:04 -0500

    Logica para la funcion de delete.html

---
+++ js/deleteMovie.js
@@ -0,0 +1,17 @@
+function closeAlertExito(){
+    const alert = document.querySelector(".alertaExito");
+    alert.style.display = block;
+}
+function closeAlertError(){
+    const alert = document.querySelector(".alertaError");
+    alert.style.display = block;
+}
+function closeConfirmacion(b){
+    const alert = document.querySelector(".confirmacion");
+    alert.style.display = block;
+    if(b){
+
+    }else{
+        
+    }
+}
